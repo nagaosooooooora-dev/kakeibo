28b7ad4d2380bc95989707331f45e811273b1f9b
nagaosooooooora-dev <nagaosooooooora@gmail.com>
2026-03-01 23:35:03 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
-// TODO: firebase-config.js をあなたの値で設定してください。
-// firebase-config.example.js をコピーして firebase-config.js にしてから編集するのがおすすめです。
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// firebase-config.js を作って、ここをあなたのFirebaseプロジェクトの値に置き換えてください。
-// Firebase Console > Project settings > General > Your apps (Web app) で取得できます。
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_AT39D4jGnnt3aTfyQhoGn0AvDpMZEhs",
+  authDomain: "kakeibo-8822f.firebaseapp.com",
+  projectId: "kakeibo-8822f",
+  storageBucket: "kakeibo-8822f.firebasestorage.app",
+  messagingSenderId: "777387799408",
+  appId: "1:777387799408:web:a805438b5d5a1656a195b5"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
